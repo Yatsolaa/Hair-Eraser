bd86455f3b3403e6c94004bba0807a6f9fda9829
Andrii Yatsola <andry.yatsola@gmail.com>
2022-07-23 19:28:52 +0300

    first commit

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,7 +1,12 @@
 "use strict";
 
-document.querySelectorAll('.accordion-item__trigger').forEach(function (item) {
-  return item.addEventListener('click', function () {
-    item.parentNode.classList.toggle('accordion-item--active');
+/* document.querySelectorAll('.accordion-item__trigger').forEach((item) => 
+    item.addEventListener('click', () => {
+        item.parentNode.classList.toggle('accordion-item--active'); 
+    })
+); */
+$(document).ready(function () {
+  $('.accordion-item__trigger').click(function () {
+    $(this).next('accordion-item__content').slideToggle();
   });
 });
--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.querySelectorAll('.accordion-item__trigger').forEach((item) => 
+/* document.querySelectorAll('.accordion-item__trigger').forEach((item) => 
     item.addEventListener('click', () => {
         item.parentNode.classList.toggle('accordion-item--active'); 
     })
-);
+); */
+
+$(document).ready(function() {
+    $('.accordion-item__trigger').click(function() {
+        $(this).next('accordion-item__content').slideToggle();
+    });
+});
